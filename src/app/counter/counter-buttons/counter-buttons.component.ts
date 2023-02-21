1f3c2c50e78eaf633abe285fa99d1ba7eb0b864c
Vineeth Serigar <vineeth.serigar@netcracker.com>
2023-02-21 11:12:12 +0530

    built a counter app using normal approach

---
+++ src/app/counter/counter-buttons/counter-buttons.component.ts
@@ -0,0 +1,24 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-buttons',
+  templateUrl: './counter-buttons.component.html',
+  styleUrls: ['./counter-buttons.component.less'],
+})
+export class CounterButtonsComponent {
+  @Output() increment = new EventEmitter<void>();
+  @Output() decrement = new EventEmitter<void>();
+  @Output() reset = new EventEmitter<void>();
+
+  onIncrement(): void {
+    this.increment.emit();
+  }
+
+  onDecrement(): void {
+    this.decrement.emit();
+  }
+
+  onReset(): void {
+    this.reset.emit();
+  }
+}
