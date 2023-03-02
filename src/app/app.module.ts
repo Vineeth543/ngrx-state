1bc9f9d94d5b299b8dd1939bb669498abb9ee07e
Vineeth Serigar <vineeth.serigar@netcracker.com>
2023-03-02 19:01:17 +0530

    added custom-serializer using ngrx custom store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { NgModule, isDevMode } from '@angular/core';
 import { AuthEffects } from './auth/state/auth.effects';
 import { BrowserModule } from '@angular/platform-browser';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { AuthTokenInterceptor } from './services/AuthToken.intercepter';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { CustomSerializer } from './store/router/custom-serializer';
 
 @NgModule({
   declarations: [AppComponent, routingComponents],
@@ -19,6 +21,9 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
     StoreModule.forRoot(AppReducer),
     EffectsModule.forRoot([AuthEffects]),
     StoreDevtoolsModule.instrument({ logOnly: !isDevMode() }),
+    StoreRouterConnectingModule.forRoot({
+      serializer: CustomSerializer,
+    }),
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthTokenInterceptor, multi: true },
--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -4,13 +4,16 @@ import { SharedReducer } from './shared/shared.reducer';
 import { AuthReducer } from '../auth/state/auth.reducer';
 import { SHARED_STATE_NAME } from './shared/shared.selector';
 import { AUTH_STATE_NAME } from '../auth/state/auth.selector';
+import { routerReducer, RouterReducerState } from '@ngrx/router-store';
 
 export interface AppState {
   [SHARED_STATE_NAME]: SharedState;
   [AUTH_STATE_NAME]: AuthState;
+  router: RouterReducerState;
 }
 
 export const AppReducer = {
   [SHARED_STATE_NAME]: SharedReducer,
   [AUTH_STATE_NAME]: AuthReducer,
+  router: routerReducer,
 };
